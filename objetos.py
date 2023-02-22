bdb8097eb79e52d1c8fa3035982fab632ac364a1
lOkY <102552151+IvanIsay@users.noreply.github.com>
2023-02-22 14:48:15 -0600

    atributos

---
+++ objetos.py
@@ -0,0 +1,9 @@
+from personaje import *
+
+#Creamos una instancia Heroe de la clase Personaje 
+Heroe= Personaje()
+
+#usamos los atributos
+print("El personaje se llama "+ Heroe.nombre)
+print("pertenece a la especie"+ Heroe.especie)
+print("y una altura de  "+ str(Heroe.altura))
--- personaje.py
+++ personaje.py
@@ -11,3 +11,12 @@ class Personaje:
             print("El personaje "+ self.nombre +" esta corriendo")
         else:
             print("El personaje "+ self.nombre +" se detuvo")
+            
+            
+    def lanzarGranadas(self):
+        print("El personaje "+ self.nombre +" lanzo granada")
+        
+    def recargarArma(self,municiones):
+        cargardor=10
+        cargardor= cargardor+ municiones
+        print("El armar recargada tiene "+ cargardor + " balas")
